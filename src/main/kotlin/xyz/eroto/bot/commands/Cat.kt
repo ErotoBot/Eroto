e2d84cb832c1fb90783f0847a6b168c1d487a48a
noud02 <noud.kerver@hotmail.nl>
2018-03-20 21:29:52 +0100

    add cat command

---
+++ src/main/kotlin/xyz/eroto/bot/commands/Cat.kt
@@ -0,0 +1,30 @@
+package xyz.eroto.bot.commands
+
+import okhttp3.Response
+import org.json.JSONObject
+import xyz.eroto.bot.entities.cmd.Command
+import xyz.eroto.bot.entities.cmd.Context
+import xyz.eroto.bot.utils.Http
+
+class Cat : Command() {
+    override val description = "Sends you a random cat"
+    override val example = "cat"
+
+    override fun run(ctx: Context) {
+        val fut = Http.get("https://aws.random.cat/meow")
+
+        fut.exceptionally {
+            ctx.send(it.toString())
+
+            Response.Builder().build()
+        }
+
+        fut.thenAccept { res ->
+            val json = JSONObject(res.body()!!.string())
+
+            ctx.send(json.getString("file"))
+
+            res.close()
+        }
+    }
+}
