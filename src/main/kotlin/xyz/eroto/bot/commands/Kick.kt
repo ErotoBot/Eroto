840b99b26af3b86c1ff73d5b436da5445338f971
Martmists <martmists@gmail.com>
2018-03-21 18:07:39 +0100

    Clean imports
    Signed-off-by: Martmists <martmists@gmail.com>

--- src/main/kotlin/xyz/eroto/bot/commands/Kick.kt
+++ src/main/kotlin/xyz/eroto/bot/commands/Kick.kt
@@ -2,7 +2,6 @@ package xyz.eroto.bot.commands
 
 import net.dv8tion.jda.core.Permission
 import net.dv8tion.jda.core.entities.Member
-import net.dv8tion.jda.core.exceptions.PermissionException
 import xyz.eroto.bot.entities.cmd.*
 
 class Kick : Command() {
@@ -30,7 +29,7 @@ class Kick : Command() {
             }
         }
 
-        if (unable.size > 0){
+        if (unable.size > 0) {
             val userFmt = "Unable to kick:\n" + unable.map { "${it.user.name}#${it.user.discriminator}" }.joinToString("\n")
             ctx.send(userFmt)
         } else {
