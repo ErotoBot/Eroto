f672ff76b744e877ffd86be5df9d376b3ba5f762
Martmists <martmists@gmail.com>
2018-03-22 17:58:42 +0100

    Do some things
    Signed-off-by: Martmists <martmists@gmail.com>

--- src/main/kotlin/xyz/eroto/bot/commands/weebsh/Hug.kt
+++ src/main/kotlin/xyz/eroto/bot/commands/weebsh/Hug.kt
@@ -1,10 +1,12 @@
 package xyz.eroto.bot.commands.weebsh
 
+import net.dv8tion.jda.core.entities.Member
 import okhttp3.Response
 import org.json.JSONObject
 import xyz.eroto.bot.Eroto
 import xyz.eroto.bot.entities.cmd.Command
 import xyz.eroto.bot.entities.cmd.Context
+import xyz.eroto.bot.entities.cmd.argument
 import xyz.eroto.bot.utils.Http
 
 private val headers = hashMapOf(
@@ -17,6 +19,10 @@ class Hug : Command() {
     override val description = "hug a user"
     override val example = "hug @user"
 
+    init {
+        arguments += argument<Member>("user")
+    }
+
     override fun run(ctx: Context) {
         val fut = Http.get("https://api.weeb.sh/images/random?type=cry", { headers.entries.map { header(it.key, it.value) } })
 
@@ -28,6 +34,10 @@ class Hug : Command() {
 
         fut.thenAccept { res ->
             val json = JSONObject(res.body()!!.string())
+
+            val target = ctx.args["user"] as Member
+
+            val msg = "${target.asMention}, you got a hug from ${ctx.author.asMention}"
 
             ctx.send(json.getString("url"))
 
--- src/main/kotlin/xyz/eroto/bot/commands/weebsh/Kiss.kt
+++ src/main/kotlin/xyz/eroto/bot/commands/weebsh/Kiss.kt
@@ -1,10 +1,12 @@
 package xyz.eroto.bot.commands.weebsh
 
+import net.dv8tion.jda.core.entities.Member
 import okhttp3.Response
 import org.json.JSONObject
 import xyz.eroto.bot.Eroto
 import xyz.eroto.bot.entities.cmd.Command
 import xyz.eroto.bot.entities.cmd.Context
+import xyz.eroto.bot.entities.cmd.argument
 import xyz.eroto.bot.utils.Http
 
 private val headers = hashMapOf(
@@ -17,6 +19,10 @@ class Kiss : Command() {
     override val description = "Kiss a user"
     override val example = "kiss @user"
 
+    init {
+        arguments += argument<Member>("user")
+    }
+
     override fun run(ctx: Context) {
         val fut = Http.get("https://api.weeb.sh/images/random?type=cry", { headers.entries.map { header(it.key, it.value) } })
 
@@ -28,6 +34,10 @@ class Kiss : Command() {
 
         fut.thenAccept { res ->
             val json = JSONObject(res.body()!!.string())
+
+            val target = ctx.args["user"] as Member
+
+            val msg = "${target.asMention}, you got a kiss from ${ctx.author.asMention}"
 
             ctx.send(json.getString("url"))
 
